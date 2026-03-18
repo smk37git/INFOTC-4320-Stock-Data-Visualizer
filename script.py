656b02f04b37ae42ac1b502c7583d870a2c1642d
miaalta <mia_alta@yahoo.com>
2026-03-17 20:40:27 -0500

    add stock symbol input function with validation

--- script.py
+++ script.py
@@ -1,1 +1,22 @@
 # Scrum Team 6 | INFOTC-4320-Stock-Data-Visualizer
+
+
+def get_stock_symbol():
+    #Ask user for stock symbol and validate input
+    #returns a valid, uppercase stock symbol
+
+    while True:
+        symbol = input("Enter the stock symbol you are looking for: ").strip().upper()
+        if len(symbol) == 0:
+            print("Invalid input! Please try again.\n")
+        elif not symbol.isalpha():
+            print("Invalid input! Stock symbols should only contain letters. Please try again.\n")
+        else:
+            return symbol
+        
+
+
+if __name__ == "__main__":
+    stock_symbol = get_stock_symbol()
+
+   
