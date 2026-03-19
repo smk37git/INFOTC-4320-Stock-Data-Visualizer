1b8b2768fb5de08aa417c236c1a2352784b3db0c
trentaduffey <trentaduffey@gmail.com>
2026-03-19 13:51:12 -0500

    Added input routing

--- script.py
+++ script.py
@@ -3,8 +3,11 @@ def SelectChart():
             try:    
                 # Ask user for input and validate their choice
                 ChartInput = int(input("Please select a chart type:\n 1. Line Graph\n 2. Bar Graph\n\n Selection: "))
-                if ChartInput == 1 or ChartInput == 2:
-                    print("Correct input")
+                while ChartInput == 1 or ChartInput == 2:
+                    if ChartInput == 1:
+                            LineGraph()
+                    elif ChartInput == 2:
+                            BarChart()
                 elif ChartInput != 1 or ChartInput != 2:
                     print("The value must be either 1 or 2")
                 else:
