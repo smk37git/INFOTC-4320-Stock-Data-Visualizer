bd4566ec240aaef5aa9d62cf9f931d43408d0a10
smk37git <sdmain3@gmail.com>
2026-03-19 14:56:01 -0500

    Added Ben Cumming's time series script and remove separate file

--- script.py
+++ script.py
@@ -35,7 +35,29 @@ def SelectChart():
             print("Input must be the integer 1 or 2")
         
 # ======= TIME SERIES (Ben) =======
+def time_series_menu():
+    
+    while True:
+    
+        print("Select the Time Series of the chart you want to Generate\n----------------------------------------------")
+        print("1. Intraday")
+        print("2. Daily")
+        print("3. Weekly")
+        print("4. Monthly")
 
+        time_series = input("Enter the time seroes option (1, 2, 3, 4): ")
+        if time_series == "1":
+            Intraday()
+        elif time_series == "2":
+            Daily()
+        elif time_series == "3":
+            Weekly()
+        elif time_series == "4":
+            Monthly()
+        else: 
+            print("ERROR: input is invalid try again...")
+            continue
+        return
 
 # ======= YYYY-MM-DD [Begin & End] (Sebastian) =======
 def get_begin_date():
@@ -106,7 +128,8 @@ def main():
     chart = SelectChart()
 
     # == Time Series ==
-
+    time_series_menu()
+    
     # == Date Selection ==
     begin_date = get_begin_date()
     end_date = get_end_date()
