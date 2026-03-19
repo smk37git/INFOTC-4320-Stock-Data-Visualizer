2da8262fe806217d84c18709cffbb4f3159faeef
trentaduffey <trentaduffey@gmail.com>
2026-03-19 13:55:42 -0500

    Syntax error correction

--- script.py
+++ script.py
@@ -5,13 +5,13 @@ def SelectChart():
                 ChartInput = int(input("Please select a chart type:\n 1. Line Graph\n 2. Bar Graph\n\n Selection: "))
                 while ChartInput == 1 or ChartInput == 2:
                     if ChartInput == 1:
-                            LineGraph()
+                        LineGraph()
                     elif ChartInput == 2:
-                            BarChart()
-                elif ChartInput != 1 or ChartInput != 2:
-                    print("The value must be either 1 or 2")
-                else:
-                    return SelectChart
+                        BarChart()
+                    elif ChartInput != 1 or ChartInput != 2:
+                        print("The value must be either 1 or 2")      
+                    else:
+                        return SelectChart
             # Making sure that a person's input is not a string
             except ValueError:
                 print("Input must be the integer 1 or 2")
