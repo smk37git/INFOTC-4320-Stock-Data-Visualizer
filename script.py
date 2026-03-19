6fccf0512554daf9a38ea8d3b319377bb7990733
miaalta <mia_alta@yahoo.com>
2026-03-19 14:57:21 -0500

    querying-api

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 # Scrum Team 6 | INFOTC-4320-Stock-Data-Visualizer
-
+import requests
 
 def get_stock_symbol():
     #Ask user for stock symbol and validate input
@@ -31,6 +31,36 @@ def SelectChart():
         # Making sure that a person's input is not a string
         except ValueError:
             print("Input must be the integer 1 or 2")
+
+def querying_api(time_series_type, stock_symbol, API_KEY):
+    while True:
+        if time_series_type == 1:
+            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_symbol}&interval=5min&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.csv()
+            return data
+        elif time_series_type == 2:
+            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.csv()
+            return data
+        elif time_series_type == 3:
+            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stock_symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.csv()
+            return data
+        elif time_series_type == 4:
+            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={stock_symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.csv()
+            return data
+        else:
+            print("Error!")
+
+
+
+
+
         
 
 def main():
