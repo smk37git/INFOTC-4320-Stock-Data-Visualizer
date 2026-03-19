001fe102fba6a10e3de166589d4c831cbffe8f2f
smk37git <sdmain3@gmail.com>
2026-03-19 14:43:23 -0500

    Added date selection; validate each date; validate begin date against end date; general script organization

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 # Scrum Team 6 | INFOTC-4320-Stock-Data-Visualizer
 
-
+# ======= STOCK SYMBOL (Mia) =======
 def get_stock_symbol():
     #Ask user for stock symbol and validate input
     #returns a valid, uppercase stock symbol
@@ -14,6 +14,8 @@ def get_stock_symbol():
         else:
             return symbol
         
+
+# ======= SELECT CHART (Trent) =======
 def SelectChart():
     while True:
         try:    
@@ -32,9 +34,82 @@ def SelectChart():
         except ValueError:
             print("Input must be the integer 1 or 2")
         
+# ======= TIME SERIES (Ben) =======
 
+
+# ======= YYYY-MM-DD [Begin & End] (Sebastian) =======
+def get_begin_date():
+
+    while True:
+
+        # Gather Input
+        begin_date = input("\nPlease enter beginning date (YYYY-MM-DD): ")
+
+        # Validate 
+        if is_date_valid(begin_date):
+            return begin_date
+
+def get_end_date():
+        
+        while True:
+
+            # Gather Input
+            end_date = input("\nPlease enter ending date (YYYY-MM-DD): ")
+
+            # Validate 
+            if is_date_valid(end_date):
+                return end_date
+
+def is_date_valid(selected_date):
+
+    # Make sure string isn't empty
+    if len(selected_date) == 0:
+            print("\nERROR: Beginning Date should not be null")
+
+    # Strip and convert to date-time format
+    try:
+        import datetime
+
+        datetime.datetime.strptime(selected_date, "%Y-%m-%d")
+        return True
+    except:
+        print("\nERROR: Input is not in correct YYYY-MM-DD format")
+        return False
+    
+def validate_dates(begin_date, end_date):
+    try:
+        import datetime
+
+        # Restrip and convert to date-time format
+        begin = datetime.datetime.strptime(begin_date, "%Y-%m-%d")
+        end = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+        # If beginning date is less than end date
+        if begin < end:
+            return True
+        else:
+            print("\nERROR: Dates are not correctly selected")
+            return False
+
+    # Date parsing failed
+    except:
+        print("\nERROR: Invalid date format")
+        return False
+
+# ============== MAIN FUNCTION ==============
 def main():
+
+    # == Stock Symbol ==
     stock_symbol = get_stock_symbol()
+
+    # == Chart Select ==
     chart = SelectChart()
+
+    # == Time Series ==
+
+    # == Date Selection ==
+    begin_date = get_begin_date()
+    end_date = get_end_date()
+    validate_dates(begin_date, end_date)
 
 main()
